4a6447b1a03455b5e62cf8ab37c00b65ba7fbb0a
MG Creations <info@mgcreations.co.za>
2020-06-06 11:02:16 +0200

    strat building user struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello Michael, great to see to again!!");
+    let user1 = User {
+        email: String::from("info@mgcreations.co.za"),
+        username: String::from("MG Creations"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{:?}", user1);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
